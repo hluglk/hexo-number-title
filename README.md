2e2a80fe7356f366612d8ebd9512bf7742565809
hluglk <641766459@qq.com>
2017-07-24 05:18:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hexo-number-title
+## hexo-number-title
+使用 uuid 作为唯一值，为每篇文章设一个不重复的id值作为 url
+
+## 安装与使用
+
+### 安装
+```bash
+npm install hexo-number-title --save
+```
+### 使用
+配置hexo根项目下的`_config.yml`
+
+```yml
+number_title:
+  enable: true
+```
+**注意**：
+第一次使用时务必初始化
+```bash
+hexo poi
+```
+
+执行 `hexo clean` 命令并不会改变 id 值，但若修改了 uuid 则会创建一个新的    
+若要初始化参考上一条
+
+### DEMO
+[VCGPGSZO](hluglk.top)
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let utils = require('./lib/util.js');
+var fs = require('hexo-fs');
+var hexo = hexo || {};
+var config = hexo.config;
+
+hexo.extend.filter.register('before_post_render', data => {
+  if (!config.number_title.enable || data.layout !== 'post') {
+    return data;
+  }
+  utils.created(data);
+})
+
+hexo.extend.console.register('poi', 'poi ?', function(args){
+  utils.poi();
+});
+
+hexo.on('exit',function(){
+  utils.saveJSON();
+})
