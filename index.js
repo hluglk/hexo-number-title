a398612cceb0795cfe1e55c2ba93cb406b25e863
hluglk <641766459@qq.com>
2017-08-29 10:46:37 +0800

    reset project

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ hexo.extend.filter.register('before_post_render', data => {
   if (config.number_title.enable) model.flush(data);
 });
 
-hexo.extend.console.register('poi', 'poi ?', args => {
-  config.number_title.isPoi = (args.f || args.flush) ? true:false;
-  if (config.number_title.enable) model.poi(config.number_title);
-});
+// hexo.extend.console.register('poi', 'poi ?', args => {
+//   if (config.number_title.enable) model.poi(config.number_title);
+// });
 
 hexo.on('new', post => {
   if (config.number_title.enable) model.new(post);
 });
 
-hexo.on('exit', () => {
-  if (config.number_title.enable) model.save(config.number_title);
-});
+// hexo.on('exit', () => {
+//   if (config.number_title.enable) model.save(config.number_title);
+// });
