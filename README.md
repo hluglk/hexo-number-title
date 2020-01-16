176877052e3e5083a3dc9671505b409bf005f096
huangshengling <hsl123!@#>
2020-01-17 03:44:58 +0800

    v2.1.0

--- README.md
+++ README.md
@@ -1,28 +1,66 @@
-## hexo-number-title
+# hexo-number-title
 use timestamp generated value for the permalink `http://hluglk.top/archives/1500846116/`
 
-## usage
-
-### install
+### INSTALL
 ```bash
 # npm
 npm install hexo-number-title --save
 # yarn 
 yarn add hexo-number-title
 ```
-### configuration
-`_config.yml`
-``` yml
-number_title:
-  enable: true
+
+### COMMAND
+```base
+❯ hexo new  test    
+INFO  Created: ~/newblog/source/_posts/test.md
+
+❯ hexo number-title -h
+Usage: hexo number-title 
+
+Description:
+hexo-number-title plugin, For all didn't set the permalink post
+
+Options:
+  -f, --force  overwrite permalink
 ```
-`scaffolds/post.md`
-```
-date: {{ date }}
-```
-### command
-```bash
-hexo n 'test'
-```
+
 ### EXAMPLE
-[VCGPGSZO](http://hluglk.top)
+
+`hexo number-title`
+```
+# before
+---
+title: test
+---
+
+# after
+---
+title: test
+permalink: 1500846110
+---
+```
+
+-----------------------
+
+`hexo number-title -f`
+```
+# before
+---
+title: test
+permalink: has-been-set-value
+---
+
+# after
+---
+title: test
+permalink: 1500846112
+---
+```
+---------------
+`hexo new test`
+```
+---
+title: test
+permalink: 1500846112
+---
+```
--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 'use strict';
 
-var model = require('./lib/model.js');
+const front = require('hexo-front-matter');
+const fs = require('hexo-fs');
 var hexo = hexo || {};
 var config = hexo.config;
 
-hexo.extend.filter.register('before_post_render', data => {
-  if (data.layout !== 'post') return data;
-  if (config.number_title.enable && config.number_title.before_post_render) model.flush(data);
+hexo.extend.console.register('number-title', 'hexo-number-title plugin, For all didn\'t set the permalink post', {
+  options: [
+    { name: '-f, --force', desc: 'overwrite permalink' }
+  ]
+}, async args => {
+  let index = new Date().getTime() / 1000;
+  hexo.extend.filter.register('before_post_render', data => {
+    if (data.layout !== 'post') return data
+
+    let lines = front.parse(data.raw);
+    if (isNaN(lines.permalink) || (args.f || args.force)) {
+      lines.permalink = parseInt(index++);
+      fs.writeFileSync(data.full_source, '---\n' + front.stringify(lines), 'utf-8');
+    }
+  });
+  
+  await hexo.call('clean');
+  await hexo.call('generate');
+  hexo.exit();
 });
 
 hexo.on('new', post => {
-  if (config.number_title.enable) model.new(post);
+  let lines = post.content.split('\n');
+  let timestamp = parseInt(new Date().getTime() / 1000);
+  let index = lines.findIndex(item => item === 'permalink:');
+  if (index > -1) {
+    lines[index] += (' ' + timestamp);
+  } else {
+    lines.splice(-2, 0, 'permalink: ' + timestamp);
+  }
+
+  post.content = lines.join('\n');
+  if (post.path !== false) {
+    fs.writeFile(post.path, post.content);
+  }
 });
